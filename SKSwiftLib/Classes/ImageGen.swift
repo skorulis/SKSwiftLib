fc3f70d03e074678685cbafa8cab13117fff7a52
Alex Skorulis <skorulis@gmail.com>
2018-08-04 16:39:52 +1000

    Add class to help generating images

---
+++ SKSwiftLib/Classes/ImageGen.swift
@@ -0,0 +1,49 @@
+//
+//  ImageGen.swift
+//  floatios
+//
+//  Created by Alexander Skorulis on 3/8/18.
+//  Copyright © 2018 Skorulis. All rights reserved.
+//
+
+import UIKit
+
+open class ImageGen: NSObject {
+    
+    public let rootDir:String
+    
+    public init(rootDir:String) {
+        self.rootDir = rootDir
+        
+        let path = PathHelper.documentsDirectoryPath() + rootDir
+        do {
+            try FileManager.default.createDirectory(atPath: path, withIntermediateDirectories: false, attributes: nil)
+        } catch {
+            
+        }
+        
+        print("Writing images to " + path)
+    }
+    
+    public func saveImage(name:String,image:UIImage) {
+        let data = UIImagePNGRepresentation(image)
+        let fileName = name + ".png"
+        let fileURL = PathHelper.documentsDirectory().appendingPathComponent(rootDir).appendingPathComponent(fileName)
+        do {
+            try data?.write(to: fileURL)
+        } catch {}
+    }
+
+    public func finishContext() -> UIImage {
+        let img = UIGraphicsGetImageFromCurrentImageContext();
+        UIGraphicsEndImageContext();
+        return img!
+    }
+    
+    public func newContext(_ size:CGSize) -> CGContext {
+        UIGraphicsBeginImageContext(size)
+        return UIGraphicsGetCurrentContext()!
+    }
+    
+}
+
